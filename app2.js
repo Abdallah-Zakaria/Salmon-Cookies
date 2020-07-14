b24f38dc4f5ec7e9b3e8002360bf7002d8d8a2ba
Your Name In Single QuotesAbdallah-Zakariagit config --global core.editor code <abdallah.zakaria1996@icloud.com>
2020-07-14 16:40:30 +0300

    add8

--- app2.js
+++ app2.js
@@ -1,1 +1,17 @@
 'use_strict';
+
+
+
+changeColorBlack = function(){
+    var head = document.getElementById("head")
+    var style = document.createElement("style") 
+    style.setAttribute("id","style")
+    head.appendChild(style)
+    style.textContent = "body{background-color: black ;color: white;} .navigatorItem{color: white} .navigatorItem:hover{color: black}"
+}
+
+changeColorNormal = function(){
+    var style = document.getElementById("style")
+    style.remove();
+}
+
