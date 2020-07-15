8fbc5655fe95f15b19f64d9d857ea1d282b9ea1e
Your Name In Single QuotesAbdallah-Zakariagit config --global core.editor code <abdallah.zakaria1996@icloud.com>
2020-07-15 14:41:10 +0300

    add9

--- app1.js
+++ app1.js
@@ -117,3 +117,23 @@ Paris.render();
 Lima.render();
 footer();
 
+
+
+var addBranch = document.getElementById("addBranchForm");
+
+addBranch.addEventListener("submit", function(){
+event.preventDefault();
+
+var branchLocation = event.target.branchLocation.value;
+console.log(branchLocation)
+
+var minimumCustomerPerHour =event.target.minimumCustomerPerHour.value;
+console.log(minimumCustomerPerHour)
+
+var maxiumumCustomerPerHour =event.target.maxiumumCustomerPerHour.value;
+console.log(maxiumumCustomerPerHour)
+
+var averageCookiesPerCustomer =event.target.averageCookiesPerCustomer.value;
+console.log(averageCookiesPerCustomer)
+
+})
